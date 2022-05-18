9a56f54ba0f5f676ae6dbccd2b86884de1200cde
mailofk <104469091+mailofk@users.noreply.github.com>
2022-05-19 00:04:42 +0900

    add_clocks

---
+++ js/clock.js
@@ -0,0 +1,7 @@
+const clock=document.querySelector("h2#clock");
+
+function getClock() {
+  clock.innerText=new Date().toLocaleTimeString();
+}
+getClock();
+setInterval(getClock,1000);
---
+++ js/greetings.js
@@ -0,0 +1,30 @@
+const LoginForm = document.querySelector("#login-form");
+const LoginInput = document.querySelector("#login-form input");
+const LoginButton = document.querySelector("#login-form button");
+const h1 = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = LoginInput.value;
+  LoginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY,username);
+  PaintGreetings(username);
+}
+
+function PaintGreetings(username) {
+  greeting.classList.remove("hidden");
+  greeting.innerText = `Nice to meet you, ${username}`;
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  LoginForm.classList.remove("hidden");
+  LoginForm.addEventListener("submit",onLoginSubmit);
+} else {
+  PaintGreetings(savedUsername);
+}
